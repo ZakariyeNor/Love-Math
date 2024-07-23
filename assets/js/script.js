158aef05971cc7ee797965d7c3ddcd8955b9e8ee
Zakariye Farah Nor <0e6xrww7@students.codeinstitute.net>
2024-07-23 17:32:43 +0000

    display addition question to the user

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,9 +12,12 @@ document.addEventListener("DOMContentLoaded", function() {
             } else {
                 let gameType = this.getAttribute("data-type");
                 alert(`You clicked ${gameType}`);
+                runGame(gameType)
             }
         })
     }
+
+    runGame("addition")
 })
 
 // Docstring (Docstrings are used to describe functions, they should go right above where the function name is declared)
@@ -24,11 +27,19 @@ document.addEventListener("DOMContentLoaded", function() {
  * and after the user's answer has been processed
  */
 
-function runGame() {
+function runGame(gameType) {
 
     //create two random numbers between 1 and 25
     let num1 = Math.floor(Math.random() * 25) + 1;
     let num2 = Math.floor(Math.random() * 25) + 1;
+
+    if (gameType === "addition") {
+        displayAdditionQuestion(num1, num2);
+    } else {
+        alert (`Unknown game type: ${gameTYpe}`);
+        throw `Unknown game tyoe: ${gameType}. Aborting!`;
+    }
+
 }
 
 function checkAnswer() {};
@@ -39,7 +50,13 @@ function incrementScore() {};
 
 function incrementWrongAnswer() {};
 
-function displayAdditionQuestion() {};
+function displayAdditionQuestion(operand1, operand2) {
+    
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = "+";
+
+};
 
 function displaySubtractQuestion() {};
 
